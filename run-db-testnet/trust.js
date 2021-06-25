6df4432c7996a143d7b4910fd0d3b5329622323b
Miguel Duarte <miguelfeliped@gmail.com>
2021-06-25 14:48:28 -0300

    using trustlist in trust script

--- run-db-testnet/trust.js
+++ run-db-testnet/trust.js
@@ -1,12 +1,12 @@
 const axios = require('axios')
 require('dotenv/config')
-const trustlist = require('./trustlist.json')
+const trustlist = require('./trustlist')
 
 const RUN_DB_URL = `http://localhost:${process.env.RUN_DB_PORT}`
 
 const main = async () => {
   await Promise.all(
-    trustlist.map(async (obj) => axios.post(`${RUN_DB_URL}/trust/${obj.location.split('_')[0]}`))
+    trustlist.all().map(async (obj) => axios.post(`${RUN_DB_URL}/trust/${obj.location.split('_')[0]}`))
   )
 }
 
